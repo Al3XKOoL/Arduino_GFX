ea891279295d8deca3df96def24e12b6c830338d
NP v/d Spek <nielsnl68@gmail.com>
2022-12-20 13:13:36 +0100

    idem

--- src/Arduino_G.h
+++ src/Arduino_G.h
@@ -18,7 +18,9 @@ public:
 
   virtual void drawBitmap(int16_t x, int16_t y, uint8_t *bitmap, int16_t w, int16_t h, uint16_t color, uint16_t bg) = 0;
   virtual void drawIndexedBitmap(int16_t x, int16_t y, uint8_t *bitmap, uint16_t *color_index, int16_t w, int16_t h) = 0;
+#ifndef DISABLE_COLOR_DEFINES 
   virtual void draw3bitRGBBitmap(int16_t x, int16_t y, uint8_t *bitmap, int16_t w, int16_t h) = 0;
+#endif
   virtual void draw16bitRGBBitmap(int16_t x, int16_t y, uint16_t *bitmap, int16_t w, int16_t h) = 0;
   virtual void draw24bitRGBBitmap(int16_t x, int16_t y, uint8_t *bitmap, int16_t w, int16_t h) = 0;
 
